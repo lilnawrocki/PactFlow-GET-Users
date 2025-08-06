8607349ceca3dcca1a27aa2068a4d2ba2b1125b4
Marcin Nawrocki <78412793+lilnawrocki@users.noreply.github.com>
2025-08-06 14:13:56 +0200

    Final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,8 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner input = new Scanner(System.in);
         String token;
         String tenant;
